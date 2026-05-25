3556fe8e0e1e7506e7309b980f3ab1c2c93ea8d5
nukhaimong <nukhaimongmarma834@gmail.com>
2026-05-25 12:26:23 +0600

    variables in go

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var txt string
+	txt = "Hello, world"
+	var firstName string = "Jekono Marma"
+	lastName := "Nu Khai"
+	var age = 55
+
+	var (
+		fatherName string = "Sr. Jekono"
+		fatherAge  int    = 60
+	)
+
+	fmt.Println(txt, ",", firstName, lastName, "my age is", age, fatherName, fatherAge)
 }
