e58f9c235964fa48ea10eb18304adb69aaf42b9a
nukhaimong <nukhaimongmarma834@gmail.com>
2026-06-12 22:11:43 +0600

    basic crud operation in golang

--- go-http-crud/main.go
+++ go-http-crud/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
+	"strconv"
 )
 
 type User struct {
@@ -35,6 +37,9 @@ func main() {
 	mux.HandleFunc("/health", healthHandler)
 	mux.HandleFunc("POST /create-user", createUserHandler)
 	mux.HandleFunc("GET /users", getUserHandler)
+	mux.HandleFunc("GET /users/{id}", getSingleUserById)
+	mux.HandleFunc("PUT /users/{id}", updateUser)
+	mux.HandleFunc("DELETE /users/{id}", deleteUser)
 
 	fmt.Println("Server is running on http://localhost:5000")
 
@@ -77,4 +82,76 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	// w.Write(users)
 	encoder := json.NewEncoder(w)
 	encoder.Encode(users)
+}
+
+func getSingleUserById(w http.ResponseWriter, r *http.Request) {
+	idParams := r.PathValue("id")
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid User Id")
+		return
+	}
+	for _, user := range users {
+		if user.Id == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User Not Found")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	idParams := r.PathValue("id")
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid User Id")
+		return
+	}
+
+	var updatedUser User
+	err = json.NewDecoder(r.Body).Decode(&updatedUser)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid request body")
+		return
+	}
+
+	for idx, user := range users {
+		if user.Id == id {
+			updatedUser.Id = id
+			users[idx] = updatedUser
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User Not Found")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	idParams := r.PathValue("id")
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid User Id")
+		return
+	}
+
+	for idx, user := range users {
+		if user.Id == id {
+			//users = append(users[:idx], users[idx+1:]...)
+			users = slices.Delete(users, idx, idx+1)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User Not Found")
 }
